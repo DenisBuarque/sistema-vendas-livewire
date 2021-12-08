8f29502ae3b3d1d59c012bdfcc1f9ce13f7b1bbb
Denis Buarque <denisprogramadorweb@gmail.com>
2021-12-08 14:33:05 -0300

    first commit

---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,58 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex">
+            <div class="flex-1">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Categoria') }}
+                </h2> 
+            </div>
+            <div class="flex-none">
+              <!-- Will not grow or shrink -->
+            </div>
+            <div class="flex-1 text-right">
+                <a href="{{ route('category.index')}}" class="rounded py-2 px-2 bg-indigo-500 text-white float-right">Listar registros</a>
+            </div>
+          </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="overflow-hidden sm:rounded-lg">
+                
+
+                  <div class="sm:mt-0">
+                    <div class="md:grid md:grid-cols-3 md:gap-6">
+                      <div class="md:col-span-1">
+                        <div class="px-4 sm:px-0">
+                          <h3 class="text-lg font-medium leading-6 text-gray-900">Adicionar categoria</h3>
+                          <p class="mt-1 text-sm text-gray-600">
+                            Os campos com * são de preenchimento obrigatório.
+                          </p>
+                        </div>
+                      </div>
+                      <div class="mt-5 md:mt-0 md:col-span-2">
+                        <form wire:submit.prevent="" action="#" method="POST">
+                          <div class="shadow overflow-hidden sm:rounded-md">
+                            <div class="px-4 py-5 bg-white sm:p-6">
+                              <div class="grid grid-cols-6 gap-6">
+
+                                @include('category.form')
+                  
+                              </div>
+                            </div>
+                            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                              <button type="submit" wire:click="" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                Salvar registro
+                              </button>
+                            </div>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/category/form.blade.php
@@ -0,0 +1,8 @@
+<div class="col-span-6 sm:col-span-3">
+    <label for="name" class="block text-sm font-medium text-gray-700">Nome da categoria:</label>  
+    <input type="text" name="name" value="{{ old('name') }}" id="name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+</div>
+<div class="col-span-6 sm:col-span-3">
+    <label for="first-name" class="block text-sm font-medium text-gray-700">Nome da categoria:</label>
+    <input type="file" name="arquivo" id="arquivo" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm">
+</div>
